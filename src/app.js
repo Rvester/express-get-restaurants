74afa174a832a4f8b5e11c69bb9d96f938910511
unknown <rasheem.khan94@gmail.comgit config --global user.name Rasheem>
2024-09-23 11:30:30 -0400

    Don't ask me to spell restauraunt

--- src/app.js
+++ src/app.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const app = express();
-const Restaurant = require("../models/index")
+const Restaurant = require("../models/index");
 const db = require("../db/connection");
 
-//TODO: Create your GET Request Route Below: 
-
-
-
+//TODO: Create your GET Request Route Below:
+app.get("/restauraunts", async (req, res) => {
+  const restauraunts = await Restaurant.findAll({});
+  res.json(restauraunts);
+});
 
 module.exports = app;
